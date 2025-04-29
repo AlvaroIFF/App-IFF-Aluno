89a1666e4375b00981f718970e8c67c605352af7
Cap-Jhotas <jonathanpquarus@gmail.com>
2025-04-28 22:41:14 -0300

    Update pop-up.js

--- App-IFF-Aluno/pop-up/pop-up.js
+++ App-IFF-Aluno/pop-up/pop-up.js
@@ -1,15 +1,11 @@
 class Popup {
   constructor(popupSelector) {
     this.popup = document.querySelector(popupSelector);
-    this.closeButton = this.popup.querySelector('.close');
     this.init();
   }
 
   // Inicializa os eventos do pop-up
   init() {
-    // Fechar pop-up ao clicar no botão de fechar
-    this.closeButton.addEventListener('click', () => this.close());
-
     // Fechar pop-up ao clicar fora dele
     window.addEventListener('click', (event) => {
       if (event.target === this.popup) {
@@ -18,20 +14,13 @@ class Popup {
     });
   }
 
-  // Método para abrir o pop-up com informações dos dias selecionados
-  open(selectedDays = []) {
-    // Verifica se foram passados dias selecionados
-    const content = selectedDays.length > 0 
-      ? `<ul>${selectedDays.map(day => `<li>${day}</li>`).join('')}</ul>`
-      : 'Nenhum dia selecionado.';
-    
-    // Atualiza o conteúdo do pop-up
-    this.popup.querySelector('.popup-content').innerHTML = `
-      <span class="close">&times;</span>
-      <h3>Dias Selecionados</h3>
-      ${content}
-    `;
-    
+  // Método para abrir o pop-up
+  open(content = '') {
+    if (content) {
+      this.popup.querySelector('.popup-content').innerHTML = `
+        ${content}
+      `;
+    }
     this.popup.classList.add('show');
   }
 
